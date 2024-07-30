4a0ed5d757ccf3d2e2f9f215a9e7be6667096f1c
Tumaini <tumainikebaya.234@gmail.com>
2024-07-30 12:32:02 +0300

    .

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,33 @@
 def write_file(file_name, file_content):
-    pass
+    
+    file_name = str(file_name)
+
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+
+    with open(file_name, 'w') as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    
+    file_name = str(file_name)
+
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+
+    with open(file_name, 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+
+    file_name = str(file_name)
+
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+
+    with open(file_name, 'r') as file:
+        return file.read()
+
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
